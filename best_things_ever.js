05e6b3f2b0aa58a9a72ad4444a5696a8cc87323a
Moh <mohakemal9@gmail.com>
2017-04-04 19:34:10 -0400

    updated with comment!
    comment was added to the original file.

--- best_things_ever.js
+++ best_things_ever.js
@@ -1,6 +1,9 @@
+//includes the FS package for reading and writing packages
 var fs = require('fs');
-
+//running the readfile module thats inside of fs.
+//store the read information into the variable "data"
 fs.readFile('best_things_ever.txt', 'utf8', function(error,data){
+	//break the string down by comma separation and store 
 	var array = data.split(',');
 	console.log('array', array);
 });
